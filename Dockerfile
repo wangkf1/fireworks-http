5e36a588aa5668c6bc5319f342b02580d408f0ad
Kevin Wang <kevinwang234@gmail.com>
2024-05-30 12:43:33 -0700

    Add cmd line options

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM ubuntu:focal
 
-RUN apt update && apt install -y libcurl4-openssl-dev clang libboost-program-options-dev
+RUN apt update && apt install -y libcurl4-openssl-dev clang libboost-program-options-dev libasio-dev
 
 WORKDIR /app
 COPY . .
 
-RUN clang++ -std=c++17 bench.cpp -I/usr/include/aarch64-linux-gnu/ -L/usr/lib/aarch64-linux-gnu/ -lcurl -o bench
+RUN clang++ -std=c++17 -O3 bench.cpp -I/usr/include/aarch64-linux-gnu/ -L/usr/lib/aarch64-linux-gnu/ -lcurl -lpthread -lboost_program_options -o bench
 ENTRYPOINT ["./bench"]
--- bench.cpp
+++ bench.cpp
@@ -5,8 +5,12 @@
 #include <unistd.h>
 #include <future>
 #include <chrono>
+#include <optional>
 
 #include <curl/curl.h>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 using namespace std::chrono;
 
 struct Result {
@@ -36,24 +40,69 @@ Result launchGetRequest(const std::string& address) {
     return result;
 }
 
-// todo: add in arg parsing
-// usage: ./bench <address> --qps <num> [--duration <secs>] [--numhandles <handles>] [--warmupDuration <secs>]
-int main() {
+struct Options {
+    std::string address;
+    int qps;
+    int duration;
+};
+
+std::optional<Options> ParseOptions(int argc, char** argv) {
+    Options options;
+
+    try {
+        // options
+        po::options_description desc("HTTP benchmarking tool");
+        desc.add_options()
+            ("help",                                                    "produce help message")
+            ("address",     po::value<std::string>(&options.address),   "HTTP address")
+            ("qps",         po::value<int>(&options.qps),               "queries per second")
+            ("duration",    po::value<int>(&options.duration)->default_value(5),"duration of test in seconds. default=5")
+        ;
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(argc, argv).
+                    options(desc).run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << "Usage: options_description [options]\n";
+            std::cout << desc;
+            return std::nullopt;
+        }
+
+        if (!vm.count("address")) {
+            std::cerr << "--address is required!\n";
+            return std::nullopt;
+        }
+        if (!vm.count("qps")) {
+            std::cerr << "--qps is required!\n";
+            return std::nullopt;
+        }
+    } catch(std::exception& e) {
+        std::cerr << "Caught exception while parsing args: " << e.what() << "\n";
+        return std::nullopt;
+    }
+    return options;
+}
+
+// usage: ./bench --address <address> --qps <num> [--duration <secs>] [--numhandles <handles>] [--warmupDuration <secs>]
+int main(int argc, char** argv) {
+    // Parse cmd line args
+    auto maybeOptions = ParseOptions(argc, argv);
+    if (maybeOptions == std::nullopt) {
+        return 1;
+    }
+    auto options = maybeOptions.value();
+
     curl_global_init(CURL_GLOBAL_DEFAULT);
-
-    // options
-    const std::string address = "http://www.google.com"; //todo: make this an arg
-    const int duration = 5;
-    const int qps = 25;
-    const int64_t usecs = (1e6 / qps);
-
+    const int64_t usecs = (1e6 / options.qps);
     std::vector<std::future<Result>> futures;
-    futures.reserve(qps * duration);
+    futures.reserve(options.qps * options.duration);
     auto start = high_resolution_clock::now();
-    for (int i = 0; i < qps*duration; ++i) {
+    for (int i = 0; i < options.qps*options.duration; ++i) {
         auto lastClock = high_resolution_clock::now();
         // launch request asynchronously
-        futures.push_back(std::async(std::launch::async, launchGetRequest, address));
+        futures.push_back(std::async(std::launch::async, launchGetRequest, options.address));
 
         // figure out how much time is left before submitting the next request
         auto usecsSinceLast = duration_cast<microseconds>(high_resolution_clock::now() - lastClock).count();
@@ -74,7 +123,7 @@ int main() {
     auto stop = high_resolution_clock::now();
 
     std::cout << "Submitted " << futures.size() << " requests over " 
-            << duration_cast<milliseconds>(submitStop - start).count() / 1000.0 << " seconds at " << qps << " qps\n";
+            << duration_cast<milliseconds>(submitStop - start).count() / 1000.0 << " seconds at " << options.qps << " qps\n";
     std::cout << "Success rate: " << 100 * (double)successes / futures.size() << "%\n";
     std::cout << "Avg request latency: " << (double)latencySum / futures.size() / 1000.0 << "ms\n";
     std::cout << "Total submit + receive time: " << duration_cast<milliseconds>(stop - start).count() / 1000.0 << " seconds\n";
